27e9ed11a517abd2c866293032182ab19188efd3
franciscapr <francisca.paredes.pr@gmail.com>
2025-05-16 16:16:57 -0400

    create user

---
+++ core/user/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.exceptions import ObjectDoesNotExist
+from djnago.db import models 
+from django.http import Http404
+
+class User(AbstractBaseUser, PermissionsMixin):
+    public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    first_name = models.Charfield(max_length=255)
+    last_name = models.Charfield(max_length=255)
+    email = models.EmailField(db_index=True, unique=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DatetimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.email}"
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class UserManager(BaseUserManager):
+    def get_object_by_public_id(self, public_id):
+        try:
+            instance = self.get(public_id=public_id)
+            return instance
+        except (ObjectDoesNotExist, ValueError, TypeError):
+            return Http404
+
+    def create_user(self, username, email, password=None, **kwargs):
+        """Create and retyurn a 'USER' with an email, phone number, username adn password."""
+        if username is None:
+            raise TypeError('Users must have a username.')
+        if email is None:
+            raise TypeError('Users must have an email address.')
+        if password is None:
+            raise TypeError('Users must have a password.')
+
+        user = self.model(username=username, email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    
