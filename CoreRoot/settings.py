49079c5a17086fd8f7a90235e497d134dd282ae0
franciscapr <francisca.paredes.pr@gmail.com>
2025-05-17 14:29:43 -0400

    migrations

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # Mis apps
     'core',
+    'core.user',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Modelo de usuario que utilizaremos para la autenticaciòn
+AUTH_USER_MODEL = 'core_user.User'
--- core/user/models.py
+++ core/user/models.py
@@ -2,31 +2,8 @@ from django.db import models
 import uuid
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.core.exceptions import ObjectDoesNotExist
-from djnago.db import models 
+from django.db import models 
 from django.http import Http404
-
-class User(AbstractBaseUser, PermissionsMixin):
-    public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(db_index=True, max_length=255, unique=True)
-    first_name = models.Charfield(max_length=255)
-    last_name = models.Charfield(max_length=255)
-    email = models.EmailField(db_index=True, unique=False)
-    is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DatetimeField(auto_now=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    objects = UserManager()
-
-    def __str__(self):
-        return f"{self.email}"
-
-    @property
-    def name(self):
-        return f"{self.first_name} {self.last_name}"
 
 
 class UserManager(BaseUserManager):
@@ -36,6 +13,7 @@ class UserManager(BaseUserManager):
             return instance
         except (ObjectDoesNotExist, ValueError, TypeError):
             return Http404
+
 
     def create_user(self, username, email, password=None, **kwargs):
         """Create and retyurn a 'USER' with an email, phone number, username adn password."""
@@ -52,4 +30,49 @@ class UserManager(BaseUserManager):
 
         return user
 
-    
+
+    def create_superuser(self, username, email, password, **kwargs):
+        """Create and return 'User' with superuser (admin) permissions."""
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+        if email is None:
+            raise TypeError('Superusers must hace an email.')
+        if username is None:
+            raise TypeError('Superusers must have a username.')
+        
+        user = self.create_user(username, email, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.email}"
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
+
