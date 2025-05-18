509408a8a5cf6449cc48ef10ea41d7b47f8a2b2e
franciscapr <francisca.paredes.pr@gmail.com>
2025-05-18 18:40:56 -0400

    añadiendo la funcionalidad de inicio de sesión

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -39,12 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # external packages apps
+    'rest_framework',
+    'rest_framework_simplejwt',
+
     # Mis apps
     'core',
     'core.user',
+    'core.auth',
 
-    # Rest
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +137,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Modelo de usuario que utilizaremos para la autenticaciòn
 AUTH_USER_MODEL = 'core_user.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': 
+        ['django_filters.rest_framework.DjangoFilterBackend'],
+}
--- core/routers.py
+++ core/routers.py
@@ -1,11 +1,18 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
+from core.auth.viewsets import RegisterViewSet, LoginViewSet
 
 router = routers.SimpleRouter()
 
-#####################   USER
+#####################   AUTH
+router.register(r'auth/register', RegisterViewSet, basename="auth-register")
+router.register(r'auth/login', LoginViewSet, basename="auth-login")
 
+#####################   USER
 router.register(r'user', UserViewSet, basename='user')
+
+
+
 
 urlpatterns = [
     *router.urls,
