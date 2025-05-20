6ff8aaa69650e0631184f88236c549fb4cdfefce
franciscapr <francisca.paredes.pr@gmail.com>
2025-05-20 16:40:53 -0400

    creando un comment

--- core/comment/models.py
+++ core/comment/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from core.abstract.models import AbstractModel, AbstractManager
+
+
+class CommentManager(AbstractManager):
+    pass
+
+
+class Comment(AbstractModel):
+    """Modelo de comentarios de publicaciones"""
+    post = models.ForeignKey("core_post.Post", on_delete=models.PROTECT)
+    author = models.ForeignKey("core_user.User", on_delete=models.PROTECT)
+
+    body = models.TextField()
+    edited = models.BooleanField(default=False)
+
+    objects = CommentManager()
+
+    def __str__(self):
+        """Definimos el método __str__ para devolver el nombre del autor"""
+        return self.author.name
