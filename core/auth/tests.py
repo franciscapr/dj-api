b717c73304618573e9bf8a6bc62ecda3d2be0a1e
franciscapr <francisca.paredes.pr@gmail.com>
2025-05-24 10:15:08 -0400

    endpoint de autenticacion

--- core/auth/tests.py
+++ core/auth/tests.py
@@ -23,6 +23,35 @@ class TestAuthenticationViewSet:
 
 
 
+    @pytest.mark.django_db
+    def test_register(self, client):
+        data = {
+            "username": "johndoe",
+            "email": "johndoe@yopmail.com",
+            "password": "test_password",
+            "first_name": "John",
+            "last_name": "Doe"
+        }
 
+        response = client.post(self.endpoint + "register/", data)
 
+        assert response.status_code == status.HTTP_201_CREATED
 
+    def test_refresh(self, client, user):
+        data = {
+            "email": user.email, # Usamos email
+            "password": "test_password"
+        }
+
+        response = client.post(self.endpoint + "login/", data)
+
+        assert response.status_code == status.HTTP_200_OK
+
+        data_refresh = {
+            "refresh": response.data['refresh']
+        }
+
+        response = client.post(self.endpoint + "refresh/", data_refresh)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['access']
