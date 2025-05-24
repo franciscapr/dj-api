f876e54973f57f9ba1ed09848cf754e05863d478
franciscapr <francisca.paredes.pr@gmail.com>
2025-05-24 10:07:12 -0400

    endpoint inicio de sesión

--- core/auth/tests.py
+++ core/auth/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import pytest
+from rest_framework import status
 
-# Create your tests here.
+from core.fixtures.user import user
+
+class TestAuthenticationViewSet:
+    
+    endpoint = '/api/auth/'
+    
+    def test_login(self, client, user):
+        """ Método de inicio de sesión """
+        data = {
+            "email": user.email, # Usamos email como inicio de la autenticación
+            "password": "test_password"
+        }
+        response = client.post(self.endpoint + "login/", data)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['access']
+        assert response.data['user']['id'] == user.public_id.hex
+        assert response.data['user']['username'] == user.username
+        assert response.data['user']['email'] == user.email
+
+
+
+
+
+
--- core/routers.py
+++ core/routers.py
@@ -10,7 +10,6 @@ router = routers.SimpleRouter()
 #####################   AUTH
 router.register(r'auth/register', RegisterViewSet, basename="auth-register")
 router.register(r'auth/login', LoginViewSet, basename="auth-login")
-
 router.register(r'auth/refresh', RefreshViewSet, basename="auth-refresh")
 
 #####################   USER
