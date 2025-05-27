0dc7e58db11c5c9ac2b4706c7a6df1b87c0400af
franciscapr <francisca.paredes.pr@gmail.com>
2025-05-27 10:34:24 -0400

    componente CreatePost

--- social-media-app/src/pages/Home.jsx
+++ social-media-app/src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
+import Layout from '../components/Layout';
 
 function Home() {
     return (
-        <div>
-            <h1>Profile</h1>
-            <p>
-                Welcome!
-                </p>
-        </div>
+        <Layout>
+            
+        </Layout>
     );
 }
 
